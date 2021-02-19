477ae3eadf68eb04fc4e82c71155063038d78bab
Yibo Yang <ybyang@seas.upenn.edu>
2021-02-19 17:28:50 -0500

    Add files via upload

---
+++ jax_bandits/build/lib/jaxbo/acquisitions.py
@@ -0,0 +1,91 @@
+import jax.numpy as np
+from jax import jit
+from jax.scipy.stats import norm
+
+# Caution: all functions are designed for single point evaluation
+# (use vmap to vectorize)
+
+@jit
+def EI(mean, std, best, xi = 0.01):
+    # from https://people.orie.cornell.edu/pfrazier/Presentations/2011.11.INFORMS.Tutorial.pdf
+    delta = -(mean - best + xi)
+    deltap = -(mean - best + xi)
+    deltap = np.clip(deltap, a_min=0.)
+    Z = delta/std
+    EI = deltap - np.abs(deltap)*norm.cdf(-Z) + std*norm.pdf(Z)
+    #EI = delta*norm.cdf(Z) + std*norm.pdf(Z)
+    return -EI[0]
+
+@jit
+def EIC(mean, std, best):
+    # Constrained expected improvement
+    delta = -(mean[0,:] - best)
+    deltap = -(mean[0,:] - best)
+    deltap = np.clip(deltap, a_min=0.)
+    Z = delta/std[0,:]
+    EI = deltap - np.abs(deltap)*norm.cdf(-Z) + std*norm.pdf(Z)
+    constraints = np.prod(norm.cdf(mean[1:,:]/std[1:,:]), axis = 0)
+    return -EI[0]*constraints[0]
+    
+    # Normalized constrainted EI
+#    mu_y = norm_const['mu_y']
+#    sigma_y = norm_const['sigma_y']
+#    delta = -(mean[0,:] - best) / sigma_y
+#    deltap = -(mean[0,:] - best) / sigma_y
+#    deltap = np.clip(deltap, a_min=0.)
+#    Z = delta/(std[0,:]/sigma_y)
+#    EI = deltap - np.abs(deltap)*norm.cdf(-Z) + std*norm.pdf(Z)
+#    constraints = np.prod(norm.cdf(mean[1:,:]/std[1:,:]), axis = 0)
+#    return -EI[0]*constraints[0]
+    
+#    xi = 0.01
+#    mu_y = norm_const['mu_y']
+#    sigma_y = norm_const['sigma_y']
+#    imp = -(mean[0,:] - best + xi) / sigma_y
+#    Z = imp / (std[0,:]/sigma_y)
+#    EI = (imp * norm.cdf(Z) + (std[0,:]/sigma_y) * norm.pdf(Z)) * np.clip(std[0,:], a_min=0.)
+#    constraints = np.prod(norm.cdf(mean[1:,:]/std[1:,:]), axis = 0)
+#    return -EI[0]*constraints[0]
+
+
+@jit
+def LCBC(mean, std, kappa = 2.0, threshold = 3.0):
+    lcb = mean[0,:] - threshold - kappa*std[0,:]
+    constraints = np.prod(norm.cdf(mean[1:,:]/std[1:,:]), axis = 0)
+    return lcb[0]*constraints[0]
+
+
+@jit
+def LW_LCBC(mean, std, weights, kappa = 2.0, threshold = 3.0):
+    lcb = mean[0,:] - threshold - kappa*std[0,:]*weights
+    constraints = np.prod(norm.cdf(mean[1:,:]/std[1:,:]), axis = 0)
+    return lcb[0]*constraints[0]
+
+@jit
+def LCB(mean, std, kappa = 2.0):
+    lcb = mean - kappa*std
+    return lcb[0]
+
+@jit
+def US(std):
+    return -std[0]
+
+@jit
+def LW_LCB(mean, std, weights, kappa = 2.0):
+    lw_lcb = mean - kappa*std*weights
+    return lw_lcb[0]
+
+@jit
+def LW_US(std, weights):
+    lw_us = std*weights
+    return -lw_us[0]
+
+@jit
+def CLSF(mean, std, kappa = 1.0):
+    acq = np.log(np.abs(mean)+1e-8) - kappa*np.log(std+1e-8)
+    return acq[0]
+
+@jit
+def LW_CLSF(mean, std, weights, kappa = 1.0):
+    acq = np.log(np.abs(mean)+1e-8) - kappa*(np.log(std+1e-8) + np.log(weights+1e-8))
+    return acq[0]
